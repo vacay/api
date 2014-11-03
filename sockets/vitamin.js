15ca741a5f2f1c055f748ce692ddb00e062a945b
t3rr0r <mail@t3rr0r.com>
2014-11-03 01:36:48 -0500

    removed logging

--- sockets/vitamin.js
+++ sockets/vitamin.js
@@ -5,8 +5,6 @@ var kue = require('kue');
 module.exports = function(io, socket, queue) {
 
     queue.on('job complete', function(id, result) {
-	console.log(id);
-	console.log(result);
 	if (result) socket.emit('vitamin:processed', result);
     });
 
